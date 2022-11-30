eda84d70ba4fc335306594b15d51000e942315a8
Bruna Quinto Marsiaj <110314990+Bruna-Quinto-Marsiaj@users.noreply.github.com>
2022-11-30 20:13:34 -0300

    observacoes ex upcast e downcast

--- src/one/digitalinnovation/oo/RodarAplicacao.java
+++ src/one/digitalinnovation/oo/RodarAplicacao.java
@@ -7,14 +7,15 @@ class RodarAplicacao {
 
     public static void main(String[] args) {
 
-        Funcionario funcionario = new Funcionario();
+        Funcionario funcionario = new Funcionario(); //sem upcast ou downcast. pegou funcionário e estanciou obj dele
 
-        Funcionario gerente = new Gerente();
-        Funcionario vendedor = new Vendedor();
-        Funcionario faxineiro = new Faxineiro();
+        Funcionario gerente = new Gerente(); //upcast. é implicito, quer transformar gerente em funcionario
+        Funcionario vendedor = new Vendedor(); //upcast facilita o polimorfismo...
+        Funcionario faxineiro = new Faxineiro(); //upcast
 
-        //Gerente gerente_ = new Funcionario();
-        Vendedor vendedor_ = (Vendedor) new Funcionario();
+        //Gerente gerente_ = new Funcionario();// forma errada de fazer pq downcast precisa ser explícito
+        Vendedor vendedor_ = (Vendedor) new Funcionario();//downcast, classe mãe, funcionario, sendo transf em filha. downcast geralmente da erro, 
+                                                          //pq perde dados, pq na subclasse tem dados q n tem na super
 
     }
 }
